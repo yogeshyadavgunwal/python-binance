b7b1883ce847d7b6541d258aa57e9805ecdaaf48
yogeshyadavgunwal <yogeshyadav4444u@gmail.com>
2021-08-16 11:14:07 +0530

    assignment
    calculate division

---
+++ yogeshwork/divison.py
@@ -0,0 +1,12 @@
+def divisonn(a, b):
+    c = (a / b) * 100
+    if c > 100 :
+        print("entered correct marks")
+    if c >= 60:
+        print("first division")
+    elif c >= 50:
+        print("second division")
+    elif c >= 33:
+        print("third division")
+    else:
+        print("you are fail")
---
+++ yogeshwork/main.py
@@ -0,0 +1,4 @@
+from divison import divisonn
+c = int(input("enter marks obtained "))
+d = int(input("enter total marks"))
+divisonn(c, d)
